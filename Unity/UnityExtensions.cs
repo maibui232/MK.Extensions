c4af5a10735947bc11c1721e5c766fdbcd65973b
duymaib <duymaibui2k@gmail.com>
2025-07-06 18:11:38 +0700

    impl kernel

--- Unity/UnityExtensions.cs
+++ Unity/UnityExtensions.cs
@@ -8,5 +8,10 @@ namespace MK.Extensions
         {
             Object.DontDestroyOnLoad(gameObject);
         }
+
+        public static Sprite ToSprite(this Texture2D texture, float pivotX = 0.5f, float pivotY = 0.5f)
+        {
+            return Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(pivotX, pivotY));
+        }
     }
 }
