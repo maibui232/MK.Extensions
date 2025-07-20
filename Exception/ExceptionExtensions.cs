f9bd4ad6437c9603279b9802c8cb5e4e446e45f0
duymaib <duymaibui2k@gmail.com>
2025-07-20 17:26:29 +0700

    wip

--- Exception/ExceptionExtensions.cs
+++ Exception/ExceptionExtensions.cs
@@ -1,13 +1,6 @@
 namespace MK.Extensions
 {
-    using System;
-    using MK.Log;
-
     public static class ExceptionExtensions
     {
-        public static void ThrowIfNotAssignable(this ILogger logger, Type type, Type baseType)
-        {
-            logger.Fatal(new Exception($"{type.FullName} is not assignable to {baseType.FullName}"));
-        }
     }
 }
